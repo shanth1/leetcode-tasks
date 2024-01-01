6d29b82abf76087633b30798c0fc8a13cc26abcd
Denis Beresnev <shanthi.bunch@gmail.com>
2024-01-01 19:39:45 +0300

    feat: Added fast solution for TwoSum

--- Easy/twoSum/index.js
+++ Easy/twoSum/index.js
@@ -5,10 +5,23 @@
  */
 
 //! O(n^2)
-var twoSum = function (nums, target) {
+var twoSumSlow = function (nums, target) {
     for (let i = 0; i < nums.length - 1; i++) {
         for (let j = i + 1; j < nums.length; j++) {
             if (nums[i] + nums[j] === target) return [i, j];
         }
     }
+};
+
+//! O(n)
+const twoSum = (nums, target) => {
+    const hashMap = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (hashMap.has(complement)) {
+            return [hashMap.get(complement), i];
+        }
+        hashMap.set(nums[i], i);
+    }
+    return null;
 };
